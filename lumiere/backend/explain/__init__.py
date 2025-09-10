1252fd6f8eaf041c719de811cef2c32765e87ec4
gabriele-marino <gabmarino.8601@gmail.com>
2025-09-10 06:45:28 +0200

    fix: get_shap_features_importance export

--- lumiere/backend/explain/__init__.py
+++ lumiere/backend/explain/__init__.py
@@ -1,7 +1,7 @@
 from lumiere.backend.explain.pdp import get_partial_dependence_values
-from lumiere.backend.explain.shapley import get_shap_values
+from lumiere.backend.explain.shapley import get_shap_features_importance
 
 __all__ = [
-    "get_shap_values",
     "get_partial_dependence_values",
+    "get_shap_features_importance",
 ]
