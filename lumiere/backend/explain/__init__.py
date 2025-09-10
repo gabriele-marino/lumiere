12edb9107eede3f5da32ff1ed32e121ceff16652
gabriele-marino <gabmarino.8601@gmail.com>
2025-09-10 06:41:18 +0200

    fix: backend explain

--- lumiere/backend/explain/__init__.py
+++ lumiere/backend/explain/__init__.py
@@ -1,15 +1,7 @@
-from lumiere.backend.explain.pdp import (
-    get_partial_dependence_values,
-    get_partial_dependence_values_distribution,
-)
-from lumiere.backend.explain.shapley import (
-    get_shap_values,
-    get_shap_values_distribution,
-)
+from lumiere.backend.explain.pdp import get_partial_dependence_values
+from lumiere.backend.explain.shapley import get_shap_values
 
 __all__ = [
     "get_shap_values",
-    "get_shap_values_distribution",
     "get_partial_dependence_values",
-    "get_partial_dependence_values_distribution",
 ]
--- lumiere/backend/explain/shapley.py
+++ lumiere/backend/explain/shapley.py
@@ -1,17 +1,15 @@
-from collections.abc import Sequence
 from functools import partial
 
 import numpy as np
 import shap  # pyright: ignore[reportMissingTypeStubs]
 from numpy.typing import ArrayLike
-from p_tqdm import p_map  # pyright: ignore
 
 from lumiere.backend import mlp
 from lumiere.backend.activation_functions import sigmoid
 from lumiere.backend.typings import ActivationFunction, Weights
 
 
-def get_shap_values(
+def get_shap_features_importance(
     weights: Weights,
     inputs: ArrayLike,
     hidden_activation: ActivationFunction = sigmoid,
@@ -26,20 +24,3 @@ def get_shap_values(
     )
     explainer = shap.Explainer(model, inputs)
     return np.mean(np.abs(explainer(inputs).values), axis=0)  # pyright: ignore
-
-
-def get_shap_values_distribution(
-    weights: Sequence[Weights],
-    inputs: ArrayLike,
-    hidden_activation: ActivationFunction = sigmoid,
-    output_activation: ActivationFunction = sigmoid,
-) -> list[list[float]]:  # shape: (n_models, n_features)
-    return p_map(
-        partial(
-            get_shap_values,
-            inputs=inputs,
-            hidden_activation=hidden_activation,
-            output_activation=output_activation,
-        ),
-        weights,
-    )
